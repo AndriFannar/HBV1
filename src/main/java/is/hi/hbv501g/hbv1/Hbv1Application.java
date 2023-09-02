d1bd56fdfe19fc62ec082530e287894a8fa7a016
Andri Fannar Kristjánsson <Andri@Hilti.is>
2023-09-02 13:23:48 +0000

    First Commit

--- src/main/java/is/hi/hbv501g/hbv1/Hbv1Application.java
+++ src/main/java/is/hi/hbv501g/hbv1/Hbv1Application.java
@@ -3,6 +3,8 @@ package is.hi.hbv501g.hbv1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is team member Andri Fannar Kristjánsson
+
 @SpringBootApplication
 public class Hbv1Application {
 
