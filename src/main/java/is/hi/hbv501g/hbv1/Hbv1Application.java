51a422b7dec7ed0cca74dfae29df3613d22192c4
ahp9 <ahp9@hi.is>
2023-09-04 15:53:08 +0000

    second commit

--- src/main/java/is/hi/hbv501g/hbv1/Hbv1Application.java
+++ src/main/java/is/hi/hbv501g/hbv1/Hbv1Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // This is team member Andri Fannar Kristjánsson
+// This is team member Ástríður Haraldsdóttir Passauer
 
 @SpringBootApplication
 public class Hbv1Application {
