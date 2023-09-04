649d5b341bafbbc8e355b93f171121a097d4d386
sog6 <sog6@hi.is>
2023-09-04 17:53:30 +0000

    third commit

--- src/main/java/is/hi/hbv501g/hbv1/Hbv1Application.java
+++ src/main/java/is/hi/hbv501g/hbv1/Hbv1Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // This is team member Andri Fannar Kristjánsson
-
+// This is team member Ástríður Haraldsdóttir Passauer
+// This is team member Sigurður Örn Gunnarsson
 @SpringBootApplication
 public class Hbv1Application {
 
