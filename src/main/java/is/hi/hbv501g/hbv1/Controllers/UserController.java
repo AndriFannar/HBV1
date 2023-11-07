bd684c1e6fe634e93f88bbc3439717ad80e81b44
ahp9 <ahp9@hi.is>
2023-11-07 17:47:39 +0000

    útlit á login, signup og patientIndex, ekkert 100%

--- src/main/java/is/hi/hbv501g/hbv1/Controllers/UserController.java
+++ src/main/java/is/hi/hbv501g/hbv1/Controllers/UserController.java
@@ -67,7 +67,6 @@ public class UserController
         if(exists != null)
         {
             session.setAttribute("LoggedInUser", exists);
-
             if(exists.isStaff())
             {
                 return "redirect:/staffIndex";
