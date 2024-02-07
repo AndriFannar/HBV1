e2d1489d7f2ff18eb1f94a7816d21330758d4363
Andri Fannar Kristjánsson <Andri@Hilti.is>
2024-02-07 13:20:55 +0000

    Trying a different API key

--- src/main/java/is/hi/hbv501g/hbv1/Configurations/AuthenticationService.java
+++ src/main/java/is/hi/hbv501g/hbv1/Configurations/AuthenticationService.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.authority.AuthorityUtils;
 public class AuthenticationService
 {
     private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
-    private static final String AUTH_TOKEN = "Test";
+    private static final String AUTH_TOKEN = "[Test]";
 
     public static Authentication getAuthentication(HttpServletRequest request)
     {
